11874235ed1864a8d934b78345e90c3c778d24cf
Arkadiusz Piekarz <piekarzarkadiusz@gmail.com>
2021-11-15 20:06:14 +0100

    Make C++ code in multiple_rows tests potentially executable

--- tests/multiple_rows.rs
+++ tests/multiple_rows.rs
@@ -8,19 +8,33 @@ use std::io::Write;
 #[test]
 fn shouldFail_whenThereIsRowAndCommaAfterItButNoSecondRow()
 {
-    let transitionTable =
-"#include <boost/msm/front/state_machine_def.hpp>
+    let transitionTable = "
+#include <boost/msm/back/state_machine.hpp>
+#include <boost/msm/front/state_machine_def.hpp>
 
-struct state1 {};
-struct state2 {};
-struct event {};
+struct State1 : public boost::msm::front::state<> {};
+struct State2 : public boost::msm::front::state<> {};
+struct Event1 {};
 
-struct Machine : public boost::msm::front::state_machine_def<Machine>
+struct MachineDef : public boost::msm::front::state_machine_def<MachineDef>
 {
+    using initial_state = State1;
+
     struct transition_table : boost::mpl::vector<
-        _row<state1, event, state2>,
+        _row<State1, Event1, State2>,
     > {};
-};";
+};
+
+using Machine = boost::msm::back::state_machine<MachineDef>;
+
+int main()
+{
+    Machine machine;
+    machine.start();
+    machine.process_event(Event1{});
+    return 0;
+}
+";
     let mut file = tempfile::NamedTempFile::new().unwrap();
     file.write_all(transitionTable.as_bytes()).unwrap();
 
@@ -31,30 +45,45 @@ struct Machine : public boost::msm::front::state_machine_def<Machine>
 #[test]
 fn shouldPass_whenTableHasTwoSimpleRows()
 {
-    let transitionTable =
-"#include <boost/msm/front/state_machine_def.hpp>
+    let transitionTable = "
+#include <boost/msm/back/state_machine.hpp>
+#include <boost/msm/front/state_machine_def.hpp>
 
-struct state1 {};
-struct state2 {};
-struct event1 {};
-struct event2 {};
+struct State1 : public boost::msm::front::state<> {};
+struct State2 : public boost::msm::front::state<> {};
+struct Event1 {};
+struct Event2 {};
 
-struct Machine : public boost::msm::front::state_machine_def<Machine>
+struct MachineDef : public boost::msm::front::state_machine_def<MachineDef>
 {
+    using initial_state = State1;
+
     struct transition_table : boost::mpl::vector<
-        _row<state1, event1, state2>,
-        _row<state2, event2, state1>
+        _row<State1, Event1, State2>,
+        _row<State2, Event2, State1>
     > {};
-};";
+};
+
+using Machine = boost::msm::back::state_machine<MachineDef>;
+
+int main()
+{
+    Machine machine;
+    machine.start();
+    machine.process_event(Event1{});
+    machine.process_event(Event2{});
+    return 0;
+}
+";
     let mut file = tempfile::NamedTempFile::new().unwrap();
     file.write_all(transitionTable.as_bytes()).unwrap();
 
     let expectedOutput =
 r"@startuml
 hide empty description
-[*] --> state1
-state1 --> state2 : on event1
-state2 --> state1 : on event2
+[*] --> State1
+State1 --> State2 : on Event1
+State2 --> State1 : on Event2
 @enduml
 ";
     assert_cmd::Command::cargo_bin(APP_NAME).unwrap().arg(file.path()).assert().success()
@@ -64,42 +93,75 @@ state2 --> state1 : on event2
 #[test]
 fn shouldPass_whenTableHasFourDifferentKindsOfRows()
 {
-        let transitionTable =
-"#include <boost/msm/front/state_machine_def.hpp>
+        let transitionTable = r#"
+#include <boost/msm/back/state_machine.hpp>
+#include <boost/msm/front/state_machine_def.hpp>
+#include <iostream>
 
-struct state1 {};
-struct state2 {};
-struct state3 {};
-struct state4 {};
-struct event1 {};
-struct event2 {};
-struct event3 {};
+struct State1 : public boost::msm::front::state<> {};
+struct State2 : public boost::msm::front::state<> {};
+struct State3 : public boost::msm::front::state<> {};
+struct State4 : public boost::msm::front::state<> {};
+struct Event1 {};
+struct Event2 {};
+struct Event3 {};
 
-struct Machine : public boost::msm::front::state_machine_def<Machine>
+struct MachineDef : public boost::msm::front::state_machine_def<MachineDef>
 {
-    void action1(const event2&);
-    void action2(const event3&);
-    bool guard1(const event2&);
-    bool guard2(const event3&);
+    void action1(const Event2&)
+    {
+        std::cout << "performing action1\n";
+    }
+
+    void action2(const Event3&)
+    {
+        std::cout << "performing action2\n";
+    }
+
+    bool guard1(const Event2&)
+    {
+        return false;
+    }
+
+    bool guard2(const Event3&)
+    {
+        return true;
+    }
+
+    using M = MachineDef;
+    using initial_state = State1;
 
     struct transition_table : boost::mpl::vector<
-         _row<state1, event1, state2>,
-        a_row<state2, event2, state3, &Machine::action1>,
-        g_row<state2, event2, state4, &Machine::guard1>,
-          row<state3, event3, state1, &Machine::action2, &Machine::guard2>
+         _row<State1, Event1, State2>,
+        a_row<State2, Event2, State3, &M::action1>,
+        g_row<State2, Event2, State4, &M::guard1>,
+          row<State3, Event3, State1, &M::action2, &M::guard2>
     > {};
-};";
+};
+
+using Machine = boost::msm::back::state_machine<MachineDef>;
+
+int main()
+{
+    Machine machine;
+    machine.start();
+    machine.process_event(Event1{});
+    machine.process_event(Event2{});
+    machine.process_event(Event3{});
+    return 0;
+}
+"#;
     let mut file = tempfile::NamedTempFile::new().unwrap();
     file.write_all(transitionTable.as_bytes()).unwrap();
 
     let expectedOutput =
 r"@startuml
 hide empty description
-[*] --> state1
-state1 --> state2 : on event1
-state2 --> state3 : on event2\ndo &Machine::action1
-state2 --> state4 : on event2\nif &Machine::guard1
-state3 --> state1 : on event3\nif &Machine::guard2\ndo &Machine::action2
+[*] --> State1
+State1 --> State2 : on Event1
+State2 --> State3 : on Event2\ndo &M::action1
+State2 --> State4 : on Event2\nif &M::guard1
+State3 --> State1 : on Event3\nif &M::guard2\ndo &M::action2
 @enduml
 ";
     assert_cmd::Command::cargo_bin(APP_NAME).unwrap().arg(file.path()).assert().success()
